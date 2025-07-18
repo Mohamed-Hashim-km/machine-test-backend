45ef57de8352e87c0a1d968ec8fe79217e01b6d0
Mohamed-Hashim-km <hashimhx3@gmail.com>
2025-07-19 04:55:41 +0530

    corseErrorFix

--- src/main.ts
+++ src/main.ts
@@ -5,9 +5,11 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: 'http://localhost:3001',
+    origin: [ 
+      'https://dns-menu-card.vercel.app/',
+    ],
   });
 
-  await app.listen(process.env.PORT ?? 3000);
+  await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
