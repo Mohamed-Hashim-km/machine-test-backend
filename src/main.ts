c3ac055dff5b06d0b8906faa11ed4bda4ce50f19
Mohamed-Hashim-km <hashimhx3@gmail.com>
2025-07-19 05:14:09 +0530

    corseErrorFix

--- src/main.ts
+++ src/main.ts
@@ -4,10 +4,9 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  app.enableCors({
-    origin: [ 
-      'https://menucard-ten.vercel.app/',
-    ],
+   app.enableCors({
+    origin: ['https://menucard-ten.vercel.app'],
+    credentials: true,
   });
 
   await app.listen(process.env.PORT || 3000);
